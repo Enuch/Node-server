49965e62e5a60f605542e1b85f79e2c7d8568032
Enuch <enuchsantys@gmail.com>
2022-06-04 02:13:24 -0300

    header, statusCode, res.end and routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <h3 align="center">Node Server<h3>
+
+<p align="center"><sub>Made by Enuch</sub></p>
--- index.js
+++ index.js
@@ -4,7 +4,23 @@ let server = http.createServer((req, res) => {
     console.log('URL: ' + req.url);
     console.log('METHOD: ' + req.method);
 
-    res.end('ok');
+    switch(req.url) {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h2>Hello World!</h2>');
+            break;
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                users: [{
+                    name: 'Enuch',
+                    id: 1
+                }]
+            }));
+            break;
+    }
 });
 
 server.listen(3000, 'localhost', () => {
