4c95872678e6a2cd701c78e7d11507207675629f
Enuch <enuchsantys@gmail.com>
2022-06-04 22:46:50 -0300

    was add database neDB and persist method for users

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+const NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = (app) => {
     app.get('/users', (req, res) => {
         res.statusCode = 200;
@@ -11,6 +17,16 @@ module.exports = (app) => {
     });
     
     app.post('/users', (req, res) => {
-        res.json(req.body);
+
+        db.insert(req.body, (error, user) => {
+            if (error) {
+                console.log(`Error: ${error}`);
+                res.status(400).json({
+                    error: error
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        })
     });
 };
